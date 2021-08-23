8a635a11547d3c42cc1be088608dafcf285a2ade
amishr19 <amishr19@jaguarlandrover.com>
2021-08-23 12:21:12 +0100

    Clean Up

--- src/main/java/com/board/task/dto/request/BoardRequest.java
+++ src/main/java/com/board/task/dto/request/BoardRequest.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Setter
 @Getter
 public class BoardRequest {
 
-  @NotEmpty
+  @NotBlank
   private String name;
 
   private String description;
--- src/main/java/com/board/task/dto/request/UserRequest.java
+++ src/main/java/com/board/task/dto/request/UserRequest.java
@@ -3,15 +3,15 @@ package com.board.task.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import java.util.Date;
 import java.util.Map;
 
 @Setter
 @Getter
 public class UserRequest {
 
-  @NotEmpty
+  @NotBlank
   private String time;
 
   @NotEmpty
